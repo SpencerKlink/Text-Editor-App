938aba55fe1c0582d509155567cbb7afaac3bd9d
Spencer Klinkowize <sklinkowize7@gmail.com>
2024-07-18 12:37:31 -0400

    updated package.json

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
+console.log('Current Directory:', __dirname);
+console.log('Serving static files from:', path.join(__dirname, '../client/dist'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
